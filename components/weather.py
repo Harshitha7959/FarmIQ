15387a8c8be2d26e32e28867ceeddc69e154467d
Harshitha7959 <705701harshitha@gmail.com>
2026-04-02 03:59:11 +0530

    Create weather.py

---
+++ components/weather.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from utils.weather_api import get_weather
+from utils.alerts import generate_alerts
+
+def show():
+
+    st.markdown("<h2>🌦 Weather Intelligence</h2>", unsafe_allow_html=True)
+
+    city = st.text_input("Enter City", "Bangalore")
+
+    if st.button("Get Weather"):
+
+        data = get_weather(city)
+
+        # -------- DATA --------
+        temp = data["temp"]
+        humidity = data["humidity"]
+        rain = data["rain"]
+
+        # -------- DISPLAY WEATHER --------
+        st.markdown(f"""
+        <div class="card">
+        🌡 Temperature: {temp}°C <br>
+        💧 Humidity: {humidity}% <br>
+        🌧 Rain Probability: {rain}%
+        </div>
+        """, unsafe_allow_html=True)
+
+        # -------- ALERT ENGINE --------
+        alerts = generate_alerts(temp, humidity, rain)
+
+        st.markdown("### ⚠️ Smart Decision Alerts")
+
+        for alert in alerts:
+
+            if alert["type"] == "warning":
+                st.warning(alert["message"])
+
+            elif alert["type"] == "info":
+                st.info(alert["message"])
+
+            elif alert["type"] == "success":
+                st.success(alert["message"])
